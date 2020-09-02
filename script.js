2efd1a17d7ed29dc635729fa5588a1eefa84db46
Sean Hopkins <seahopki12@gmail.com>
2020-09-02 12:44:37 -0400

    Added five questions, styled accordingly, and started delcaring variables, event listener, and functions

---
+++ script.js
@@ -0,0 +1,13 @@
+var questionOne = document.getElementById("#questionOne");
+var questionTwo = document.getElementById("#questionTwo");
+var questionThree = document.getElementById("#questionThree");
+var questionFour = document.getElementById("#questionFour");
+var questionFive = document.getElementById("#questionFive");
+var begin = document.getElementById("#begin");
+var questionArray = [questionOne, questionTwo, questionThree, questionFour, questionFive]
+
+function startQuiz () {
+    
+}
+
+begin.addEventListener("click", startQuiz);
