d6672f6a967faf768375cc77ed19675f7f22bdc3
Sean Hopkins <seahopki12@gmail.com>
2020-09-02 22:12:46 -0400

    Cleaned up HTML, CSS, and JS files to improve efficiency and clarity

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-var questionOne = document.getElementById("#questionOne");
-var questionTwo = document.getElementById("#questionTwo");
-var questionThree = document.getElementById("#questionThree");
-var questionFour = document.getElementById("#questionFour");
-var questionFive = document.getElementById("#questionFive");
-var begin = document.getElementById("#begin");
-var questionArray = [questionOne, questionTwo, questionThree, questionFour, questionFive]
-
-function startQuiz () {
+$(document).ready(function(){
+    $("#begin").on("click", function(){
+        $("#intro").hide();
+        $("#questionContainer").css("visibility",  "visible");
+        $(".btn").on("click", function(){
+            // try a for loop to call a new set of questions and answers when a previous answer is selected
+            $("#questions").text("Question #2: How many days are in a year?")
+            $(".a").text("300")
+            $(".b").text("365")
+            $(".c").text("387")
+            $(".d").text("323")
+            // add evaluation and here and alert if answer is right or wrong
+        })
+    });
     
-}
-
-begin.addEventListener("click", startQuiz);
+});
